a645f90105035d7b3738d52ba9b3ac1a6c60fd9e
parthggote <86362454+parthggote@users.noreply.github.com>
2026-04-26 18:46:24 +0530

    Org logo fix

--- apps/officer-portal/next.config.ts
+++ apps/officer-portal/next.config.ts
@@ -9,7 +9,16 @@ const nextConfig: NextConfig = {
     '@workspace/dtos',
     '@workspace/routes',
     '@workspace/ui'
-  ]
+  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
